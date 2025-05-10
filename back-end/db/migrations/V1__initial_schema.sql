69df4dcbf0263c58b11d5ede640cbd23e4d27c8e
Vusi Pekane <vusumuzi@bbd.co.za>
2025-05-10 15:38:45 +0200

    modified migrations

--- back-end/db/migrations/V1__initial_schema.sql
+++ back-end/db/migrations/V1__initial_schema.sql
@@ -4,35 +4,32 @@ CREATE TABLE accounts(
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-CREATE TABLE games(
-  game_id SERIAL PRIMARY KEY,
-  game VARCHAR NOT NULL,
-  description VARCHAR
+CREATE TABLE age_groups(
+  age_group_id SERIAL PRIMARY KEY,
+  minimum_age INT NOT NULL,
+  maximum_age INT NOT NULL
 );
-
-CREATE TABLE difficulty_levels(
-  difficulty_id SERIAL PRIMARY KEY,
-  game_id INT NOT NULL,
-  difficulty_level VARCHAR NOT NULL,
-  CONSTRAINT fk_game_id FOREIGN KEY (game_id) REFERENCES games(game_id),
-  CONSTRAINT unique_game_difficulty UNIQUE (game_id, difficulty_level) -- prevent duplicate difficulty names per game
-);
-
-CREATE TABLE categories(
-  category_id SERIAL PRIMARY KEY,
-  category VARCHAR NOT NULL,
-  -- description VARCHAR
-);
-
 
 CREATE TABLE profiles(
   profile_id SERIAL PRIMARY KEY,
   account_id INT NOT NULL,
   username VARCHAR NOT NULL,
-  age INT NOT NULL,
+  age_group_id INT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_account_id FOREIGN KEY(account_id) REFERENCES accounts(id)
+  CONSTRAINT fk_account_id FOREIGN KEY(account_id) REFERENCES accounts(id),
+  CONSTRAINT fk_age_group_id FOREIGN KEY(age_group_id) REFERENCES age_groups(age_group_id)
+);
+
+CREATE TABLE categories(
+  category_id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  description VARCHAR
+);
+
+CREATE TABLE games(
+  game_id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  description VARCHAR
 );
 
 CREATE table words(
@@ -55,16 +52,13 @@ CREATE TABLE session_logs(
   profile_id INT NOT NULL,
   game_id INT NOT NULL,
   category_id INT NOT NULL,
-  difficulty_level_id INT NOT NULL,
   started_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   ended_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   score INT,
   CONSTRAINT fk_profile_id FOREIGN KEY(profile_id) REFERENCES profiles(profile_id),
   CONSTRAINT fk_game_id FOREIGN KEY(game_id) REFERENCES games(game_id),
-  CONSTRAINT fk_category_id FOREIGN KEY(category_id) REFERENCES categories(category_id),
-  CONSTRAINT fk_difficulty_level_id FOREIGN KEY(difficulty_level_id) REFERENCES difficulty_levels(difficulty_id)
+  CONSTRAINT fk_category_id FOREIGN KEY(category_id) REFERENCES categories(category_id)
 );
-
 
 CREATE TABLE rewards(
   reward_id SERIAL PRIMARY KEY,
