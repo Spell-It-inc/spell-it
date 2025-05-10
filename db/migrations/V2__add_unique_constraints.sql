fb064e88ad9bfd00af23981e970d99d1262fa176
Michael Holmes <yaosile@Michaels-M1-NEW-MacBook-Air.local>
2025-05-10 16:57:43 +0200

    test

--- db/migrations/V2__add_unique_constraints.sql
+++ db/migrations/V2__add_unique_constraints.sql
@@ -4,6 +4,6 @@ ALTER TABLE words ADD CONSTRAINT uk_words_word UNIQUE(word_id);
 
 ALTER TABLE games ADD CONSTRAINT uk_games_game UNIQUE(game_id);
 
-ALTER TABLE profiles ADD CONSTRAINT uk_profile_username UNIQUE(username_id);
+ALTER TABLE profiles ADD CONSTRAINT uk_profile_username UNIQUE(profile_id);
 
 ALTER TABLE rewards ADD CONSTRAINT uk_rewards_reward UNIQUE(name);
