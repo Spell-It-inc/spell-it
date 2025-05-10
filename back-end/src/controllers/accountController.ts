4051eacbce04563103f688d903ce862addf17aff
Michael Holmes <yaosile@Michaels-M1-NEW-MacBook-Air.local>
2025-05-10 15:44:07 +0200

    test

--- back-end/src/controllers/accountController.ts
+++ back-end/src/controllers/accountController.ts
@@ -3,8 +3,7 @@ import { AccountModel } from "../models/account";
 
 export class AccountController {
   static async getAccountById(req: Request, res: Response) {
-    console.error("testing error")
-    console.log("testing log")
+
     try {
       const accountId = parseInt(req.params.id);
 
