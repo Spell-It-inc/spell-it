a45ac4d42e12afa3ffe4ab2d602b3a2b79a432ea
Michael Holmes <yaosile@Michaels-M1-NEW-MacBook-Air.local>
2025-05-10 15:28:07 +0200

    test

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
