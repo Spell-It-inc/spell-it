822f005e7f74a5c5877b118add33e492ab4144e3
ndzalama.mabasa <ndzalama.mabasa@bbd.co.za>
2025-05-11 12:08:33 +0200

    fix route to get all profiles

--- back-end/src/routes/profileRoutes.ts
+++ back-end/src/routes/profileRoutes.ts
@@ -4,7 +4,7 @@ import { validateProfileData } from "../middleware/profileValidator";
 
 const router = Router();
 
-router.get("/", ProfileController.getEarnedRewards);
+router.get("/", ProfileController.getAllProfiles);
 router.get("/:id", ProfileController.getProfileById);
 router.post("/", validateProfileData, ProfileController.createProfile);
 router.put("/:id", validateProfileData, ProfileController.updateProfile);
