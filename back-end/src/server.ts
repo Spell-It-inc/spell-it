296e40d78fe058db5c4c339138b2b68bbc1bc325
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-05-14 15:30:14 +0200

    added logging

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config({ path: 'local.env' });
 
 const port = process.env.PORT || 8080;
-try {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-} catch (err) {
-  console.log(err)
-}
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}).on('error', err => {
+  console.error(err)
+}).on('close', (msg: any) => {
+  console.log('The server has closed ' + msg.toString())
+});
