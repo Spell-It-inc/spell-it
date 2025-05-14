cb5ecaae9fa44a5e0ebb837b3477572587e77feb
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-05-14 14:23:02 +0200

    test back-end env

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -6,15 +6,18 @@ import wordRoutes from "./routes/wordRoutes";
 import categoryRoutes from "./routes/categoryRoutes";
 import gameRoutes from "./routes/gameRoutes";
 import cors from "cors";
+import { validateGoogleProfile } from "./middleware/verifyGoogleAuth";
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://spellit.s3-website.af-south-1.amazonaws.com",
+  origin: process.env.REDIRECT_URI,
   credentials: true
 }));
+
+// app.use('/api', validateGoogleProfile);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/profiles", profileRoutes);
@@ -22,6 +25,7 @@ app.use("/api/accounts", accountRoutes);
 app.use("/api/words", wordRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/games", gameRoutes);  
+app.use("/api/auth", authRoutes);  
 
 
 export default app;
