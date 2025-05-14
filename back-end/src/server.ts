40ed530ecb9fa6713bd8fc470f98c91e628696d8
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-05-14 15:10:56 +0200

    test

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,11 @@
 import app from "./app";
 
 const port = process.env.PORT || 8080;
-
+try {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+}catch (err) {
+  console.log(err)
+}
