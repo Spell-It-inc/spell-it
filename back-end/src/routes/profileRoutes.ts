575d0606c3d2ef3f7ef4bffdc49711a23e7c7b95
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-05-15 19:40:40 +0200

    removed google auth for now

--- back-end/src/routes/profileRoutes.ts
+++ back-end/src/routes/profileRoutes.ts
@@ -5,9 +5,9 @@ import { verifyGoogleAuth } from "../middleware/verifyGoogleAuth";
 
 const router = Router();
 
-router.get("/", verifyGoogleAuth, ProfileController.getAllProfiles);
+router.get("/", ProfileController.getAllProfiles);
 router.get("/:id", ProfileController.getProfileById);
-router.post("/", verifyGoogleAuth, validateProfileData, ProfileController.createProfile);
+router.post("/", ProfileController.createProfile);
 router.get("/:id/session-logs", ProfileController.getProfileSessionLogs);
 
 export default router;
